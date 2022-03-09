d66c9a45064108031ebd658244f414d14c65179e
MatthewL09 <matthew.r.luna09@gmail.com>
2022-03-09 10:55:38 -0600

    first

---
+++ acquire.py
@@ -0,0 +1,21 @@
+# Wrangle
+
+# Modules (acquire.py + prepare.py)
+
+# Module(s) with user-defined functions for acquiring and preparing the data should be created.
+
+# Each function contains a helpful docstring explaining what it does, its input(s) and output(s).
+
+# Credentials (such as in an env.py file) are NOT included in the public repo.
+
+# For example:
+
+# test acquire function
+# add to acquire.py module
+# write code to clean data in notebook
+# merge code into a single function & test
+# write code to split data in notebook
+# merge code into a single function & test
+# merge functions in a single function & test
+# Add all 3 functions (or more) to prepare.py file
+# import into notebook and test functions
